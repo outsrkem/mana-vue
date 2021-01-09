83fa232a18d61892a58bf8fa058a12175fe11e69
Outsrkem <xhdascnf@126.com>
2021-01-09 15:27:15 +0800

    用户-退出登录

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -20,7 +20,14 @@
           </div>
           <el-dropdown-menu slot="dropdown">
             <el-dropdown-item>设置</el-dropdown-item>
-            <el-dropdown-item>退出</el-dropdown-item>
+            <!--
+              组件默认是不识别原生事件的，除非内部做了处理
+              https://cn.vuejs.org/v2/guide/components-custom-events.html#%E5%B0%86%E5%8E%9F%E7%94%9F%E4%BA%8B%E4%BB%B6%E7%BB%91%E5%AE%9A%E5%88%B0%E7%BB%84%E4%BB%B6
+             -->
+            <el-dropdown-item
+              @click.native="onLogout"
+            >退出
+            </el-dropdown-item>
           </el-dropdown-menu>
         </el-dropdown>
       </el-header>
@@ -61,6 +68,27 @@ export default {
     loadUserInfo () {
       getUserInfo().then(res => {
         this.user = res.data.response
+      })
+    },
+
+    onLogout () {
+      // MessageBox 弹框
+      this.$confirm('确认退出吗？', '退出提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        // 把用户的登录状态清除
+        window.localStorage.removeItem('user')
+
+        // 跳转到登录页面
+        this.$router.push('/login')
+      }).catch(() => {
+        // 取消的弹窗
+        // this.$message({
+        //   type: 'info',
+        //   message: '已取消退出'
+        // })
       })
     }
   }
