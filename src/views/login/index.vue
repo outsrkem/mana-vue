ee3c2e0faa71052b30cb54d7684ae3b212f763d9
Outsrkem <xhdascnf@126.com>
2021-02-09 21:05:12 +0800

    登录界面的input框优化，影藏密码，回车事件

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,36 +3,23 @@
     <!--
       el-form 表单组件
       每个表单项都必须使用 el-form-item 组件包裹
-     -->
-
+    -->
     <div class="login-form-wrap">
       <div class="login-head">
         <div class="logo"></div>
       </div>
       <el-form class="login-form" ref="form" :model="user">
         <el-form-item>
-          <el-input
-            v-model="user.username"
-            placeholder="请输入手机号"
-          ></el-input>
+          <el-input v-model="user.username" prefix-icon='el-icon-user' clearable placeholder="请输入登录名"/>
         </el-form-item>
         <el-form-item>
-          <el-input
-            v-model="user.password"
-            placeholder="请输入验证码"
-          ></el-input>
+          <el-input v-model="user.password" prefix-icon='el-icon-lock' clearable show-password @keyup.enter.native="onLogin" placeholder="请输入密码"/>
         </el-form-item>
         <el-form-item>
           <el-checkbox v-model="checked">我已阅读并同意用户协议和隐私条款</el-checkbox>
         </el-form-item>
         <el-form-item>
-          <el-button
-            class="login-btn"
-            type="primary"
-            :loading="loginLoading"
-            @click="onLogin"
-          >登录
-          </el-button>
+          <el-button class="login-btn" type="primary" :loading="loginLoading" @click="onLogin">登录</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -51,31 +38,29 @@ export default {
     return {
       loginLoading: false, // 登录的 loading 状态
       user: {
-        username: 'admin', // 手机号
-        password: '123456' // 验证码
+        username: 'admin', // 登录名
+        password: '123456' // 密码
       },
-      checked: false // 是否同意协议的选中状态
+      checked: true // 是否同意协议的选中状态
     }
   },
   computed: {},
   watch: {},
-  created () {
-  },
-  mounted () {
-  },
+  created () {},
+  mounted () {},
   methods: {
     onLogin () {
-      // 获取表单数据（根据接口要求绑定数据）
-      // const user = this.user
-
-      // 表单验证
-
-      // 验证通过，提交登录
-      // 对于代码中的请求操作
-      // 1、接口请求可能需要重用
-      // 2、实际工作中，接口非常容易变动，改起来麻烦
-      // 我们建议的做法是把所有的请求都封装成函数然后统一的组织到模块中进行管理
-      // 这样做的好处就是：管理维护更方便，也好重用
+      /**
+       * 获取表单数据（根据接口要求绑定数据）
+       * const user = this.user
+       * 表单验证
+       * 验证通过，提交登录
+       * 对于代码中的请求操作
+       * 1、接口请求可能需要重用
+       * 2、实际工作中，接口非常容易变动，改起来麻烦
+       * 我们建议的做法是把所有的请求都封装成函数然后统一的组织到模块中进行管理
+       * 这样做的好处就是：管理维护更方便，也好重用
+       */
       this.loginLoading = true
       login(this.user).then(res => {
         // 登录成功
