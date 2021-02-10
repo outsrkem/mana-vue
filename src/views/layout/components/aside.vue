864a3ab55b1970753ebe1253c45f6749d71b71a6
Outsrkem <xhdascnf@126.com>
2021-02-11 00:47:48 +0800

    给菜单添加图标

--- src/views/layout/components/aside.vue
+++ src/views/layout/components/aside.vue
@@ -6,34 +6,32 @@
   <div class="el-aside">
     <el-menu :default-active="activePath" router unique-opened>
       <el-menu-item index="/" @click="onSaveNavState('/')">
-        <i class="el-icon-setting"></i>
-        <span slot="title">首页</span>
+        <i class="el-icon-setting"></i><span slot="title">首页</span>
       </el-menu-item>
-      <!-- 可折叠 -->
+      <!-- 可折叠导航链接 -->
       <el-submenu index="/link">
-        <template slot="title">
-          <i class="el-icon-link"></i>
-          <span>导航链接</span>
-        </template>
-          <el-menu-item index="/link/list" @click="onSaveNavState('/link/list')">链接浏览</el-menu-item>
-          <el-menu-item index="/link/edit" @click="onSaveNavState('/link/edit')">链接编辑</el-menu-item>
-          <el-menu-item index="/link/create" @click="onSaveNavState('/link/create')">添加链接</el-menu-item>
+        <template slot="title"><i class="el-icon-link"></i> <span>导航链接</span></template>
+          <el-menu-item index="/link/list" @click="onSaveNavState('/link/list')">
+            <i class="el-icon-menu"></i><span slot="title">链接浏览</span>
+          </el-menu-item>
+          <el-menu-item index="/link/edit" @click="onSaveNavState('/link/edit')">
+            <i class="el-icon-menu"></i><span slot="title">链接编辑</span>
+          </el-menu-item>
+          <el-menu-item index="/link/create" @click="onSaveNavState('/link/create')">
+            <i class="el-icon-menu"></i><span slot="title">添加链接</span>
+          </el-menu-item>
       </el-submenu>
       <el-menu-item index="/hostmonitor" @click="onSaveNavState('/hostmonitor')">
-        <i class="el-icon-stopwatch"></i>
-        <span slot="title">主机监控</span>
+        <i class="el-icon-stopwatch"></i><span slot="title">主机监控</span>
       </el-menu-item>
       <el-menu-item index="/servicemanager" @click="onSaveNavState('/servicemanager')">
-        <i class="el-icon-mouse"></i>
-        <span slot="title">服务管理</span>
+        <i class="el-icon-mouse"></i><span slot="title">服务管理</span>
       </el-menu-item>
       <el-menu-item index="/cluster" @click="onSaveNavState('/cluster')">
-        <i class="el-icon-box"></i>
-        <span slot="title">集群管理</span>
+        <i class="el-icon-box"></i><span slot="title">集群管理</span>
       </el-menu-item>
       <el-menu-item index="/settings" @click="onSaveNavState('/settings')">
-        <i class="el-icon-menu"></i>
-        <span slot="title">个人设置</span>
+        <i class="el-icon-menu"></i><span slot="title">个人设置</span>
       </el-menu-item>
     </el-menu>
   </div>
