c027b580a7566afc248370f1d32be5329e3e103b
Outsrkem <xhdascnf@126.com>
2021-02-10 22:56:34 +0800

    优化链接展示界面

--- src/views/navigation/content/links.vue
+++ src/views/navigation/content/links.vue
@@ -1,146 +1,59 @@
 <template>
   <div>
-    <el-row :gutter="20">
-      <el-col :span="8">
-        <div class="grid-content bg-purple">
-          <!-- 卡片 -->
-          <el-card class="box-card" shadow="hover">
-            <div slot="header" class="clearfix">
-              <span>公共网址</span>
-              <el-button size="mini" icon="el-icon-refresh" style="float: right; padding: 7px 15px;" :loading="refreshLoading" @click="onRefresh"/>
-            </div>
-            <div class="text item" v-for="(data,index) in links" :key="index">
-              <div class="link-list-line" v-if="data.category==='1'">
-                <div style="" class="link-list-line-name">
-                  <el-link target="_blank" :href="data.content" >{{ data.name }}</el-link>
-                </div>
-                <el-link type="primary" target="_blank" :href="data.content">{{ data.content | UrlSnippet }}</el-link>
-              </div>
-            </div>
-          </el-card>
-        </div>
-      </el-col>
-      <el-col :span="8">
-        <div class="grid-content bg-purple">
-          <!-- 卡片 -->
-          <el-card class="box-card" shadow="hover">
-            <div slot="header" class="clearfix">
-              <span>监控网址</span>
-              <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
-            </div>
-            <div class="text item" v-for="(data,index) in links" :key="index">
-              <div class="link-list-line" v-if="data.category==='2'">
-                <div style="" class="link-list-line-name">
-                  <el-link target="_blank" :href="data.content" >{{ data.name }}</el-link>
-                </div>
-                <el-link type="primary" target="_blank" :href="data.content">{{ data.content | UrlSnippet }}</el-link>
-              </div>
-            </div>
-          </el-card>
-        </div>
-      </el-col>
-      <el-col :span="8">
-        <div class="grid-content bg-purple">
-          <!-- 卡片 -->
-          <el-card class="box-card" shadow="hover">
-            <div slot="header" class="clearfix">
-              <span>办公网址</span>
-              <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
-            </div>
-            <div class="text item" v-for="(data,index) in links" :key="index">
-              <div class="link-list-line" v-if="data.category==='3'">
-                <div style="" class="link-list-line-name">
-                  <el-link target="_blank" :href="data.content" >{{ data.name }}</el-link>
-                </div>
-                <el-link type="primary" target="_blank" :href="data.content">{{ data.content | UrlSnippet }}</el-link>
-              </div>
-            </div>
-          </el-card>
-        </div>
-      </el-col>
-    </el-row>
-    <el-row :gutter="20">
-      <el-col :span="8">
-        <div class="grid-content bg-purple">
-          <!-- 卡片 -->
-          <el-card class="box-card" shadow="hover">
-            <div slot="header" class="clearfix">
-              <span>业务网址</span>
-              <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
-            </div>
-            <div class="text item" v-for="(data,index) in links" :key="index">
-              <div class="link-list-line" v-if="data.category==='4'">
-                <div style="" class="link-list-line-name">
-                  <el-link target="_blank" :href="data.content" >{{ data.name }}</el-link>
-                </div>
-                <el-link type="primary" target="_blank" :href="data.content">{{ data.content | UrlSnippet }}</el-link>
-              </div>
-            </div>
-          </el-card>
-        </div>
-      </el-col>
-      <el-col :span="8">
-        <div class="grid-content bg-purple">
-          <!-- 卡片 -->
-          <el-card class="box-card" shadow="hover">
-            <div slot="header" class="clearfix">
-              <span>其他</span>
-              <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
-            </div>
-            <div class="text item" v-for="(data,index) in links" :key="index">
-              <div class="link-list-line" v-if="data.category==='5'">
-                <div style="" class="link-list-line-name">
-                  <el-link target="_blank" :href="data.content" >{{ data.name }}</el-link>
-                </div>
-                <el-link type="primary" target="_blank" :href="data.content">{{ data.content | UrlSnippet }}</el-link>
-              </div>
-            </div>
-          </el-card>
-        </div>
-      </el-col>
-      <el-col :span="8">
-        <div class="grid-content bg-purple">
-          <!-- 卡片 -->
-          <el-card class="box-card" shadow="hover">
-            <div slot="header" class="clearfix">
-              <span>卡片名称</span>
-              <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
-            </div>
-            <div class="text item" v-for="(data,index) in links" :key="index">
-              <div class="link-list-line" v-if="data.category==='6'">
-                <div style="" class="link-list-line-name">
-                  <el-link target="_blank" :href="data.content" >{{ data.name }}</el-link>
-                </div>
-                <el-link type="primary" target="_blank" :href="data.content">{{ data.content | UrlSnippet }}</el-link>
-              </div>
-            </div>
-          </el-card>
-        </div>
-      </el-col>
-    </el-row>
+    <div class="link-category-container">
+      <el-row :gutter="20">
+        <el-col :span="24">
+          <div class="link-category">
+            <span>链接类型：</span>
+            <el-button type="text" @click="onCategoryValue()">全部</el-button>
+            <span v-for="(item, index) in categoryOptions" :key="index">
+              <el-button type="text" @click="onCategoryValue(index)">{{item}}</el-button>
+            </span>
+          </div>
+        </el-col>
+      </el-row>
+    </div>
+    <div class="link-url-container" style="">
+        <el-row :gutter="20">
+          <el-col :span="3" v-for="(link,index) in linksCategoryFiltered" :key="index">
+            <el-link :href="link.content" target="_blank" type="primary">{{ link.name | nameSnippet }}</el-link>
+          </el-col>
+        </el-row>
+    </div>
   </div>
 </template>
 
 <script>
 import { getLinksAll } from '@/api/navigation'
 export default {
-  // 指定 name 选项的另一个好处是便于调试。
-  // 有名字的组件有更友好的警告信息。
-  // 另外，当在有 vue-devtools，未命名组件将显示成 <AnonymousComponent>，这很没有语义。通过提供 name 选项，可以获得更有语义信息的组件树。
-  // 结论：给一个组件起个名字是非常有意义的，尽量不要让组件的名字重复
   name: 'NavigationLinks',
   components: {},
   data () {
     return {
       links: [],
+      categoryOptions: {
+        1: '公共网址',
+        2: '监控网址',
+        3: '办公网址',
+        4: '业务网址',
+        5: '其他'
+      },
+      categoryValue: '',
       refreshLoading: false
     }
   },
-  computed: {},
+  computed: {
+    // 按类别过滤
+    linksCategoryFiltered: function () {
+      return this.links.filter((links) => {
+        return links.category.match(this.categoryValue)
+      })
+    }
+  },
   filters: {
     // url snippet
-    UrlSnippet: function (value) {
-      const len = 30
+    nameSnippet: function (value) {
+      const len = 8
       if (value.length < len) {
         return value
       }
@@ -149,6 +62,7 @@ export default {
   },
   watch: {},
   created () {
+    this.loadNavigationLinksAll()
   },
   mounted () {
   },
@@ -167,6 +81,9 @@ export default {
       // 刷新页面
       this.refreshLoading = true
       this.loadNavigationLinksAll()
+    },
+    onCategoryValue (value = '') {
+      this.categoryValue = value
     }
   }
 }
@@ -178,5 +95,27 @@ export default {
     .link-list-line-name {
       width:200px;
     }
+  }
+  .link-category-container {
+    margin: auto;
+    width: 80%;
+    line-height: 36px;
+    border-bottom: 1px solid #c1c3c7;
+    .link-category {
+      border-radius: 4px;
+      min-height: 36px;
+      span {
+        padding-left: 20px;
+      }
+      .el-button {
+        margin-left: 26px;
+      }
+    }
+  }
+  .link-url-container {
+    margin: auto;
+    width: 90%;
+    line-height: 36px;
+    margin-top:20px;
   }
 </style>
