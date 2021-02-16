45049a510aa27a21e3f6b4a1ea37420a3269aefb
Outsrkem <xhdascnf@126.com>
2021-02-16 10:51:44 +0800

    开启gzip打包

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,23 @@
+const webpack = require('webpack')
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+const productionGzipExtensions = ['js', 'css']
 module.exports = {
+  /** 开启gzip压缩，优化打包 */
+  configureWebpack: {
+    plugins: [
+      new CompressionWebpackPlugin({
+        algorithm: 'gzip',
+        test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+        threshold: 10240,
+        minRatio: 0.8
+      }),
+      new webpack.optimize.LimitChunkCountPlugin({
+        maxChunks: 5,
+        minChunkSize: 100
+      })
+    ]
+  },
+  /** 开发环境代理 */
   publicPath: process.env.NODE_ENV === 'production' ? './' : './',
   lintOnSave: false,
   productionSourceMap: false,
